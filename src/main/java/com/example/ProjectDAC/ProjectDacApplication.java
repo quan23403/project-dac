b46b7bf0489f373699550faf42ad0ef4d927d163
quan23403 <quan23403@gmail.com>
2024-11-05 12:09:09 +0700

    login register

---
+++ src/main/java/com/example/ProjectDAC/ProjectDacApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectDAC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDacApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/ProjectDAC/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.ProjectDAC.controller;
+
+import com.example.ProjectDAC.domain.User;
+import com.example.ProjectDAC.error.IdInvalidException;
+import com.example.ProjectDAC.response.ResCreateUserDTO;
+import com.example.ProjectDAC.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello";
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<ResCreateUserDTO> createUser(@Valid @RequestBody User user) throws IdInvalidException {
+        boolean isEmailExist = this.userService.isEmailExist(user.getEmail());
+        if(isEmailExist) {
+            throw new IdInvalidException("Email da ton tai" );
+        }
+        String hashPassword = this.passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashPassword);
+        ResCreateUserDTO newUser = this.userService.create(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+
+}
